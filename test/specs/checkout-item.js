5fb2b940bb92f0a5e25e115aa86992b6acd9be3c
amaralll <amaralzidane@gmail.com>
2025-05-05 16:16:50 +0700

    fix checkout item

--- test/specs/checkout-item.js
+++ test/specs/checkout-item.js
@@ -34,6 +34,45 @@ describe("Checkout item in cart", () => {
       );
       await addtoCart_btn.click();
       await browser.pause(5000);
+
+      await cart_badge.isDisplayed();
+
+      const cart_btn = await $('//*[@id="shopping_cart_container"]/a');
+
+      await cart_btn.click();
+
+      const cart_qty = await $(
+        '//*[@id="cart_contents_container"]/div/div[1]/div[3]/div[1]'
+      );
+
+      expect(cart_qty).toHaveText("1");
+      await browser.pause(2000);
+
+      const checkout_btn = await $(".checkout_button");
+      await checkout_btn.click();
+
+      await $('//*[@id="first-name"]').setValue("John");
+      await $('//*[@id="last-name"]').setValue("Doe");
+      await $('//*[@id="postal-code"]').setValue("12345");
+      await browser.pause(2000);
+      const continue_btn = await $(
+        '//*[@id="checkout_info_container"]/div/form/div[2]/input'
+      );
+      await continue_btn.click();
+
+      const checkout_overview = await $('//*[@id="contents_wrapper"]/div[2]');
+      expect(checkout_overview).toHaveText("Checkout: Overview");
+
+      await browser.pause(5000);
+      const finish_btn = await $(
+        '//*[@id="checkout_summary_container"]/div/div[2]/div[8]/a[2]'
+      );
+      await finish_btn.click();
+
+      const order_complete = await $(
+        '//*[@id="checkout_complete_container"]/h2'
+      );
+      expect(order_complete).toHaveText("THANK YOU FOR YOUR ORDER");
     }
   });
 });
